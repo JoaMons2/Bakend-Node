e817eac3792f9b28164d47013c17912a4582337c
JoaMons2 <ejoannamonserrat@gmail.com>
2023-05-30 11:43:27 -0600

    Get_recibirParametros

--- index.js
+++ index.js
@@ -8,14 +8,37 @@ app.get('/', (req, res) => {
 
 app.get('/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
- });
+});
 
 app.get('/products', (req, res) => {
-  res.json({
-    name: 'Producto 1',
-    price: 1000
+  res.json([
+    {
+      name: 'Producto 1',
+      price: 1000
+    },
+    {
+      name: 'Producto 2',
+      price: 2000
+    }
+  ]);
 });
- });
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id,
+    name: 'Producto 2',
+    price: 2000
+  });
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId} = req.params;
+  res.json({
+    categoryId,
+    productId,
+  })
+})
 
 app.listen(port, () => {
   console.log('Mi port' + port);
