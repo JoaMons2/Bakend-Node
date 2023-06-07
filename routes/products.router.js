c29404fc91a671cb8e2187859bb0f7cdab94384f
JoaMons2 <ejoannamonserrat@gmail.com>
2023-06-06 20:11:53 -0600

    PUT_PATCH_DELETE

--- routes/products.router.js
+++ routes/products.router.js
@@ -30,13 +30,31 @@ router.get('/:id', (req, res) => {
     price: 2000
   });
 });
-
 router.post('/', (req, res) => {
   const body = req.body;
   res.json({
     message: 'created',
-    data: body
+    data: body,
   });
 });
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'update',
+    data: body,
+    id,
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: 'deleted',
+    id,
+  });
+});
+
 
 module.exports = router;
